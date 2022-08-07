d4311b4210d4195895e3f230362275dca5bb72b7
heerajsaini <heerajsaini@gmail.com>
2022-08-07 17:22:04 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className="row">
+            <div className="col-12">
+                <p>
+                    This is a sample <b> HOME </b> page just to demonstrate routing demo.
+                </p>
+            </div>
+        </div>
+
+    );
+
+}
+
+export { Home };
